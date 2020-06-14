28bd1beee2aec0b9ef59a98ccdbfee7183ab2a3b
AdamBauman <adam@kungfutreachery.net>
2020-06-14 11:58:33 -0500

    Starting to receive responses!

--- simplessm.py
+++ simplessm.py
@@ -11,28 +11,46 @@ class SelectMonitor:
     def __init__(self, port):
         self.serial = serial.Serial(
             port, baudrate = 4800,
-            bytesize = serial.EIGHTBITS, parity = serial.PARITY_EVEN,
-            stopbits = serial.STOPBITS_ONE,  timeout = 1.0
+            bytesize = serial.EIGHTBITS, parity = serial.PARITY_NONE,
+            stopbits = serial.STOPBITS_ONE, timeout = 1.0
         )
-        if not self.serial.is_open():
+        if not self.serial.is_open:
+            print("Error opening serial port")
             raise Exception("Open serial connection")
 
     def __del__(self):
-        if self.serial.is_open():
+        if self.serial.is_open:
             self.serial.close()
+            
+    def __get_hex_string__(self, byte_data):
+        hex_string = ""
+        for single_byte in byte_data:
+            hex_string += "{:#04x} ".format(single_byte)
+            
+        return hex_string
+        
 
     def test_command(self, command):
-
-        print("Writing command: {}".format(command.hex(' ')))
+        print("Command to send: {}".format(self.__get_hex_string__(command)))
+        print("Writing command...")
         self.serial.write(command)
-        print("Finished writing command")
-
+        
+        write_wait_count = 0
+        while 0 != self.serial.out_waiting:
+            time.sleep(0.1)
+            write_wait_count = write_wait_count + 1
+            if 10 < write_wait_count:
+                print("Error, wait count hit while sending!")
+                break
+        
+        ("Finished writing command")
+        
         print("Waiting for response...")
-        bytes_waiting = self.serial.in_waiting()
+        bytes_waiting = self.serial.in_waiting
         wait_count = 0 # TODO: Replace with proper timeout check
         while 0 == bytes_waiting:
             time.sleep(0.1)
-            bytes_waiting = self.serial.in_waiting()
+            bytes_waiting = self.serial.in_waiting
             wait_count = wait_count + 1
             if 10 < wait_count:
                 print("Error, no response!")
@@ -41,6 +59,6 @@ class SelectMonitor:
         if 0 != bytes_waiting:
             print("Received response, bytes in waiting: {}".format(bytes_waiting))
             received_bytes = self.serial.read(bytes_waiting)
-            print("Received bytes: {}".format(received_bytes.hex(' ')))
+            print("Received bytes:  {}".format(self.__get_hex_string__(received_bytes)))
 
 
--- test.py
+++ test.py
@@ -4,29 +4,46 @@ import struct
 from simplessm import SelectMonitor
 
 class Configuration:
-    #port_name = "/dev/ttyUSB0"
-    port = "COM1"
+    port_name = "/dev/ttyUSB0"
+    #port = "COM1"
 
 def main():
     config = Configuration()
-    ssm = SelectMonitor(config.port)
+    ssm = SelectMonitor(config.port_name)
 
     # 0x80:dest:src:datasize:command:datamultibytes:checksum
-    #example_command = struct.pack(
-    #    "!BBBBBB", 
-    #    0x80, 0x10, 0xF0, 0x01, 0xBF, 0x40
-    #)
+    
+#     print("\nECU init...")
+#     example_command = struct.pack(
+#         "!BBBBBB", 
+#         0x80, 0x10, 0xF0, 0x01, 0xBF, 0x40
+#     )
+#     ssm.test_command(example_command)
 
     #P0x008 = coolant temp (Subtract 40 to get Degrees C)
     #P0x01C = battery voltage (multiply by 0.02 to get volts)
 
-    # Should return 0x7D and 0XB1
-    example_command = struct.pack(
-        "!BBBBBBBBBBBBB",
-        0x80, 0x10, 0xF0, 0x08, 0xA8, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x1c, 0x54
-    )
+    ssm_header = 0x80
+    ssm_dest_ecu = 0x10
+    ssm_src_diagtool = 0xF0
 
+    print("\nRead coolant temp and battery voltage...")
+    #Should return 0x7D and 0XB1
+    #       0x80, 0x10, 0xF0, 0x08, 0xA8, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x1C, 0x54
+    example_command = struct.pack(
+       "!BBBBBBBBBBBBB",
+        ssm_header, ssm_dest_ecu, ssm_src_diagtool, 0x08, 0xA8, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x1C, 0x54
+    )
     ssm.test_command(example_command)
+    
+    print("\nTest reading a MEMORY block...")
+    example_command = struct.pack(
+        "!BBBBBBBBBBB",
+        ssm_header, ssm_dest_ecu, ssm_src_diagtool, 0X06, 0XA0, 0X00, 0X20, 0X00, 0x00, 0X7F, 0XC5
+    )
+    ssm.test_command(example_command)
+
+
 
 
 if __name__ == "__main__":
